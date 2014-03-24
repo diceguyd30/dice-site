b148170c71becc0a38061c5ba26091ced686a782
Michael Ennis <michael@ennismightier.com>
2014-03-24 09:05:57 -0400

    adding more sites

---
+++ DiceCatalog/index.js
@@ -0,0 +1,26 @@
+var path = require("path");
+var express = require("express");
+var app = express();
+
+app.use(express.logger('dev'));
+app.use(express.static(path.join(__dirname, "views")));
+app.use(express.urlencoded());
+app.set('views', path.join(__dirname, "views"));
+
+app.engine("html", require("ejs").renderFile);
+
+app.get("/", 
+    function(req, res) {
+        res.render("main.html");    
+    }
+);
+
+app.get("*", 
+    function(req, res)
+    {
+        res.send("404 Not Found");
+    }
+);
+
+app.listen(process.env.PORT);
+console.log("Listening on port " + process.env.PORT);
