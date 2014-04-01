7bd070f057619e51d6c77a8467f77c5517843d94
Michael Ennis <michael@ennismightier.com>
2014-04-01 14:13:58 -0400

    still testing deploy

--- DiceCatalog/index.js
+++ DiceCatalog/index.js
@@ -1,5 +1,5 @@
 var sys = require('sys');
-var exec = require('child_process').execFile;
+var exec = require('child_process').exec;
 var path = require("path");
 var express = require("express");
 var app = express();
@@ -35,7 +35,7 @@ app.get("/deploy",
             if (error) { sys.puts(error); }
             sys.puts(stdout);
         }
-        exec("deploy", puts);
+        exec("sudo sh -c '/var/www/DiceCatalog/dice-site/DiceCatalog/deploy'", puts);
     }
 );
 
