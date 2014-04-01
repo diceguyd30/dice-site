61c6ba893a68eab092816475a8935d957e94cfcd
Michael Ennis <michael@ennismightier.com>
2014-04-01 10:58:10 -0400

    more changes to deploy script

--- DiceCatalog/index.js
+++ DiceCatalog/index.js
@@ -1,5 +1,5 @@
 var sys = require('sys');
-var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
 var path = require("path");
 var express = require("express");
 var app = express();
@@ -35,7 +35,7 @@ app.get("/deploy",
             if (error) { sys.puts(error); }
             sys.puts(stdout);
         }
-        spawn("sudo /var/www/DiceCatalog/dice-site/DiceCatalog/deploy", puts);
+        exec("/var/www/DiceCatalog/dice-site/DiceCatalog/deploy", puts);
     }
 );
 
