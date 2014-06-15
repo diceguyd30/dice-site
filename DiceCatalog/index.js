e41846f91b223312e24db7dd9ff58c0ca05122b3
Michael Ennis <michael@ennismightier.com>
2014-06-15 14:11:21 -0400

    added config module and beginner data classes

--- DiceCatalog/index.js
+++ DiceCatalog/index.js
@@ -1,13 +1,7 @@
-var sys = require('sys');
-var exec = require('child_process').exec;
+var config = require("./config");
 var path = require("path");
 var express = require("express");
 var app = express();
-
-//Set up the port variable if we aren't in the c9.io IDE
-if (!process.env.PORT) {
-    process.env.PORT = 8080;
-}
 
 app.use(express.logger('dev'));
 app.use(express.static(path.join(__dirname, "views")));
@@ -18,7 +12,7 @@ app.engine("html", require("ejs").renderFile);
 
 app.get("/", 
     function(req, res) {
-        res.render("placeholder.html");    
+        res.render("main.html");    
     }
 );
 
@@ -29,5 +23,5 @@ app.get("*",
     }
 );
 
-app.listen(process.env.PORT);
-console.log("Listening on port " + process.env.PORT);
+app.listen(config.PORT);
+console.log("Listening on port " + config.PORT);
