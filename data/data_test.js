215cf6f650a5882f1f4fbcf36cf79b1b29ce5333
Michael Ennis <michael@ennismightier.com>
2014-09-15 03:35:15 +0000

    fixed unit test

--- data/data_test.js
+++ data/data_test.js
@@ -1,12 +1,12 @@
 //Unit tests for the data module
 
-exports.testSomething = function(test){
+exports["testSomething"] = function(test){
     test.expect(1);
     test.ok(true, "this assertion should pass");
     test.done();
 };
 
-exports.testSomethingElse = function(test){
-    test.ok(false, "this assertion should fail");
+exports["testSomethingElse"] = function(test){
+    test.ok(3 == 3, "this assertion should also pass");
     test.done();
 };
