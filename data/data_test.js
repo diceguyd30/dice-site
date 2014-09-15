9676f1f66b9a871024ca1a6e403dc94b57733dc9
Michael Ennis <michael@ennismightier.com>
2014-09-15 03:24:54 +0000

    added nodeunit testing module

---
+++ data/data_test.js
@@ -0,0 +1,12 @@
+//Unit tests for the data module
+
+exports.testSomething = function(test){
+    test.expect(1);
+    test.ok(true, "this assertion should pass");
+    test.done();
+};
+
+exports.testSomethingElse = function(test){
+    test.ok(false, "this assertion should fail");
+    test.done();
+};
